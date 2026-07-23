108d90ee358e740963538a2b89a5ce0189d30a9c
cevente <cjgrenlund@gmail.com>
2026-07-23 09:40:52 +0800

    Add UDFPS handler implementation in Rust
    Signed-off-by: cevente <cjgrenlund@gmail.com>

---
+++ udfps/src/lib.rs
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2024 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![cfg_attr(not(feature = "std"), no_std)]
+
+#[cfg(feature = "std")]
+extern crate std;
+
+// Module declarations
+mod bindings;
+mod display;
+mod handler;
+mod monitors;
+mod touch;
+
+// Re-exports
+pub use handler::XiaomiSm6225UdfpsHandler;
+
+use core::ffi::c_void;
+use core::ptr;
+
+// Initialize Android logging
+#[cfg(target_os = "android")]
+#[no_mangle]
+pub extern "C" fn RustInit() {
+    android_logger::init_once(
+        android_logger::Config::default()
+            .with_tag("UdfpsHandler.rs")
+            .with_max_level(log::LevelFilter::Info)
+    );
+}
+
+// C ABI factory functions
+#[no_mangle]
+pub extern "C" fn create_udfps_handler() -> *mut c_void {
+    log::info!("Creating UDFPS handler (Rust implementation)");
+    let handler = XiaomiSm6225UdfpsHandler::new();
+    let boxed: Box<dyn handler::UdfpsHandler> = Box::new(handler);
+    Box::into_raw(boxed) as *mut c_void
+}
+
+#[no_mangle]
+pub extern "C" fn destroy_udfps_handler(handler: *mut c_void) {
+    if !handler.is_null() {
+        log::info!("Destroying UDFPS handler (Rust implementation)");
+        unsafe { drop(Box::from_raw(handler as *mut dyn handler::UdfpsHandler)) };
+    }
+}
+
+// C ABI factory struct
+#[repr(C)]
+pub struct UdfpsHandlerFactory {
+    pub create: unsafe extern "C" fn() -> *mut c_void,
+    pub destroy: unsafe extern "C" fn(*mut c_void),
+}
+
+#[no_mangle]
+pub static UDFPS_HANDLER_FACTORY: UdfpsHandlerFactory = UdfpsHandlerFactory {
+    create: create_udfps_handler,
+    destroy: destroy_udfps_handler,
+};
+
+// Ensure the struct layout matches C++ expectations
+#[cfg(target_os = "android")]
+#[no_mangle]
+pub extern "C" fn udfps_handler_factory() -> *const UdfpsHandlerFactory {
+    &UDFPS_HANDLER_FACTORY
+}
