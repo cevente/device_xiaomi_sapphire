6b2b6f3a78d329268cf2e07e896b965ac5671a98
cevente <cjgrenlund@gmail.com>
2026-07-23 10:01:55 +0800

    Simplify build script and add dummy bindings generation
    Updated build script to create a dummy bindings file if the actual bindings do not exist, simplifying the process without using bindgen.
    
    Signed-off-by: cevente <cjgrenlund@gmail.com>

--- udfps/build.rs
+++ udfps/build.rs
@@ -1,56 +1,114 @@
-// build.rs - Generates Rust bindings from C headers
+// build.rs - Simpler version without bindgen
 
 use std::env;
+use std::fs;
 use std::path::PathBuf;
 
 fn main() {
-    println!("cargo:rerun-if-changed=bindings/wrapper.h");
-    println!("cargo:rerun-if-changed=include/xiaomi_touch.h");
-    
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    
-    // Generate bindings from C headers
-    let bindings = bindgen::Builder::default()
-        .header("bindings/wrapper.h")
-        .clang_arg("-Iinclude")
-        .clang_arg("-Iexternal/libcxx/include")
-        .clang_arg("-Isystem/core/libcutils/include")
-        .clang_arg("-Ihardware/interfaces/biometrics/fingerprint/4.0/default")
-        .allowlist_type("ModeType")
-        .allowlist_type("disp_base")
-        .allowlist_type("disp_event_req")
-        .allowlist_type("disp_event_resp")
-        .allowlist_type("disp_local_hbm_req")
-        .allowlist_type("fingerprint_device_t")
-        .allowlist_var("TOUCH_.*")
-        .allowlist_var("THP_.*")
-        .allowlist_var("MI_DISP_.*")
-        .allowlist_var("LHBM_.*")
-        .allowlist_var("COMMAND_.*")
-        .allowlist_var("PARAM_.*")
-        .allowlist_var("FOD_STATUS_.*")
-        .allowlist_var("MAX_BUF_SIZE")
-        .allowlist_var("SET_CUR_VALUE")
-        .allowlist_var("GET_CUR_VALUE")
-        .allowlist_var("TOUCH_MAGIC")
-        .blocklist_type(".*__u8")
-        .blocklist_type(".*__u16")
-        .blocklist_type(".*__u32")
-        .blocklist_type(".*__u64")
-        .blocklist_type(".*__s8")
-        .blocklist_type(".*__s16")
-        .blocklist_type(".*__s32")
-        .blocklist_type(".*__s64")
-        .no_layout_tests()
-        .generate()
-        .expect("Unable to generate bindings");
-    
-    bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
-    
+    // Just link the required libraries
     println!("cargo:rustc-link-search=native=/system/lib64");
     println!("cargo:rustc-link-lib=log");
     println!("cargo:rustc-link-lib=cutils");
     println!("cargo:rustc-link-lib=base");
+    
+    // Create a dummy bindings file if needed
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let bindings_path = out_path.join("bindings.rs");
+    
+    if !bindings_path.exists() {
+        fs::write(&bindings_path, 
+            "// Auto-generated dummy bindings\n\
+             #![allow(non_camel_case_types)]\n\
+             #![allow(non_snake_case)]\n\
+             #![allow(dead_code)]\n\
+             \n\
+             // Constants from xiaomi_touch.h\n\
+             pub type ModeType = i32;\n\
+             pub const Touch_Fod_Enable: ModeType = 10;\n\
+             pub const THP_FOD_DOWNUP_CTL: ModeType = 1001;\n\
+             pub const TOUCH_IOC_SET_CUR_VALUE: u32 = 0x40045400;\n\
+             pub const MAX_BUF_SIZE: usize = 256;\n\
+             \n\
+             // Display constants\n\
+             pub const MI_DISP_PRIMARY: i32 = 0;\n\
+             pub const MI_DISP_IOCTL_SET_LOCAL_HBM: u32 = 0xC00C4601;\n\
+             pub const MI_DISP_IOCTL_REGISTER_EVENT: u32 = 0xC00C4602;\n\
+             pub const MI_DISP_EVENT_FOD: i32 = 1;\n\
+             pub const LOCAL_HBM_UI_READY: i32 = 1;\n\
+             pub const LHBM_TARGET_BRIGHTNESS_WHITE_1000NIT: i32 = 1;\n\
+             pub const LHBM_TARGET_BRIGHTNESS_OFF_FINGER_UP: i32 = 0;\n\
+             \n\
+             // FOD constants\n\
+             pub const COMMAND_NIT: i32 = 10;\n\
+             pub const PARAM_NIT_FOD: i32 = 1;\n\
+             pub const PARAM_NIT_NONE: i32 = 0;\n\
+             pub const COMMAND_FOD_PRESS_STATUS: i32 = 1;\n\
+             pub const PARAM_FOD_PRESSED: i32 = 1;\n\
+             pub const PARAM_FOD_RELEASED: i32 = 0;\n\
+             pub const FOD_STATUS_OFF: i32 = 0;\n\
+             pub const FOD_STATUS_ON: i32 = 1;\n\
+             \n\
+             // Paths\n\
+             pub const TOUCH_DEV_PATH: &str = \"/dev/xiaomi-touch\";\n\
+             pub const DISP_FEATURE_PATH: &str = \"/dev/mi_display/disp_feature\";\n\
+             pub const FOD_PRESS_STATUS_PATH: &str = \"/sys/class/touch/touch_dev/fod_press_status\";\n\
+             pub const BRIGHTNESS_PATH: &str = \"/sys/class/backlight/panel0-backlight/brightness\";\n\
+             \n\
+             // Thread periods\n\
+             pub const POLL_TIMEOUT_MS: i32 = 1000;\n\
+             pub const CLEANUP_DELAY_MS: u64 = 500;\n\
+             pub const FORCE_CLEANUP_DELAY_MS: u64 = 3000;\n\
+             \n\
+             // AcquiredInfo\n\
+             #[repr(i32)]\n\
+             #[derive(Debug, Clone, Copy, PartialEq, Eq)]\n\
+             pub enum AcquiredInfo {\n\
+                 Good = 0,\n\
+                 Partial = 1,\n\
+                 Insufficient = 2,\n\
+                 SensorDirty = 3,\n\
+                 TooSlow = 4,\n\
+                 TooFast = 5,\n\
+                 Immobile = 6,\n\
+                 Vendor = 7,\n\
+             }\n\
+             \n\
+             // Fingerprint device\n\
+             #[repr(C)]\n\
+             pub struct fingerprint_device_t {\n\
+                 pub ext_cmd: Option<unsafe extern \"C\" fn(*mut fingerprint_device_t, i32, i32) -> i32>,\n\
+                 _private: [u8; 0],\n\
+             }\
+             \n\
+             // Display structures\n\
+             #[repr(C)]\n\
+             #[derive(Debug, Clone, Copy)]\n\
+             pub struct disp_base {\n\
+                 pub flag: u32,\n\
+                 pub disp_id: i32,\n\
+             }\n\
+             \n\
+             #[repr(C)]\n\
+             #[derive(Debug, Clone, Copy)]\n\
+             pub struct disp_event_req {\n\
+                 pub base: disp_base,\n\
+                 pub type_: i32,\n\
+             }\n\
+             \n\
+             #[repr(C)]\n\
+             #[derive(Debug, Clone, Copy)]\n\
+             pub struct disp_event_resp {\n\
+                 pub base: disp_base,\n\
+                 pub type_: i32,\n\
+                 pub data: [i32; 10],\n\
+             }\n\
+             \n\
+             #[repr(C)]\n\
+             #[derive(Debug, Clone, Copy)]\n\
+             pub struct disp_local_hbm_req {\n\
+                 pub base: disp_base,\n\
+                 pub local_hbm_value: i32,\n\
+             }"
+        ).expect("Failed to write bindings.rs");
+    }
 }
