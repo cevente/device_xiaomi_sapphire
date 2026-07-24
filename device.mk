dccbae9b81fc97f1fe7988b648a4abd920624f22
cevente <cjgrenlund@gmail.com>
2026-07-24 17:11:36 +0800

    Add audio policy configuration file to product copy
    Signed-off-by: cevente <cjgrenlund@gmail.com>

--- device.mk
+++ device.mk
@@ -87,6 +87,7 @@ PRODUCT_PACKAGES += \
     libsndcardparser
 
 PRODUCT_COPY_FILES += \
+    $(AUDIO_HAL_DIR)/configs/common/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml
     $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/audio_policy_configuration.xml \
     $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/configs/audio/kvh2xml.xml:$(TARGET_COPY_OUT_VENDOR)/etc/kvh2xml.xml
