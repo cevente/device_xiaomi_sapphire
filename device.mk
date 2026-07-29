ee5a450cfc7d27d74958e1edac749b684894318c
cevente <cjgrenlund@gmail.com>
2026-07-29 20:08:59 +0800

    Remove qtipower mode_ext_lib configuration
    Signed-off-by: cevente <cjgrenlund@gmail.com>

--- device.mk
+++ device.mk
@@ -470,7 +470,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 $(call soong_config_set,power_libperfmgr,mode_extension_lib,//$(LOCAL_PATH):libperfmgr-ext-xiaomi)
-$(call soong_config_set,qtipower,mode_ext_lib,//$(LOCAL_PATH):libpowermode-ext-sapphire)
 $(call soong_config_set,qtipower,tap_to_wake_node,/proc/tp_gesture)
 
 # QCC
