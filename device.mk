aa047cc5b1bce807e6f449a7ca213e520810524b
cevente <cjgrenlund@gmail.com>
2026-07-29 19:24:12 +0800

    Add additional Soong namespaces to device.mk
    Signed-off-by: cevente <cjgrenlund@gmail.com>

--- device.mk
+++ device.mk
@@ -543,6 +543,10 @@ PRODUCT_SHIPPING_API_LEVEL := $(BOARD_SHIPPING_API_LEVEL)
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel \
+    hardware/lineage/interfaces/power-libperfmgr \
+    hardware/qcom-caf/common/libqti-perfd-client \
     hardware/xiaomi
 
 # Telephony
