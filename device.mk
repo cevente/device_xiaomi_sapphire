20d0e1c3366d0d58b21cce36287c0bda9d582cd6
cevente <cjgrenlund@gmail.com>
2026-07-29 19:22:12 +0800

    Update power service packages in device.mk
    Signed-off-by: cevente <cjgrenlund@gmail.com>

--- device.mk
+++ device.mk
@@ -464,8 +464,8 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
-    android.hardware.power@1.2.vendor
+    android.hardware.power-service.lineage-libperfmgr \
+    libqti-perfd-client
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
