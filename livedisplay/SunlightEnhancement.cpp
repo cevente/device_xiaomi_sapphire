d724bf5d67e38f5681247d7cd2c77af3ecf37d18
cevente <cjgrenlund@gmail.com>
2026-08-08 05:47:26 +0800

    Refactor SunlightEnhancement to AIDL interface
    Refactor SunlightEnhancement class to use AIDL interface and remove direct file operations.
    
    Signed-off-by: cevente <cjgrenlund@gmail.com>

--- livedisplay/SunlightEnhancement.cpp
+++ livedisplay/SunlightEnhancement.cpp
@@ -3,39 +3,24 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#define LOG_TAG "SunlightEnhancementService"
+#pragma once
 
-#include <android-base/file.h>
-#include <android-base/logging.h>
-#include <android-base/strings.h>
-
-#include "SunlightEnhancement.h"
+#include <aidl/vendor/lineage/livedisplay/BnSunlightEnhancement.h>
 
 namespace aidl {
 namespace vendor {
 namespace lineage {
 namespace livedisplay {
 
-static constexpr const char* kDispCommandPath = 
-        "/proc/mi_display/tx_cmd_set_prim";
+class SunlightEnhancement : public BnSunlightEnhancement {
+  public:
+    // Methods from ::aidl::vendor::lineage::livedisplay::BnSunlightEnhancement follow.
+    ndk::ScopedAStatus getEnabled(bool* _aidl_return) override;
+    ndk::ScopedAStatus setEnabled(bool enabled) override;
 
-ndk::ScopedAStatus SunlightEnhancement::getEnabled(bool* _aidl_return) {
-    std::string buf;
-    if (!android::base::ReadFileToString(kDispCommandPath, &buf)) {
-        LOG(ERROR) << "Failed to read " << kDispCommandPath;
-        return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
-    }
-    *_aidl_return = std::stoi(android::base::Trim(buf)) == 28;
-    return ndk::ScopedAStatus::ok();
-}
-
-ndk::ScopedAStatus SunlightEnhancement::setEnabled(bool enabled) {
-    if (!android::base::WriteStringToFile((enabled ? "28" : "34"), kDispCommandPath)) {
-        LOG(ERROR) << "Failed to write " << kDispCommandPath;
-        return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
-    }
-    return ndk::ScopedAStatus::ok();
-}
+  private:
+    bool mEnabled = false;
+};
 
 }  // namespace livedisplay
 }  // namespace lineage
