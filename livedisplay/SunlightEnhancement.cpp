06ff188ea871360f20ce735b54fab693508ab1c4
cevente <cjgrenlund@gmail.com>
2026-08-08 05:48:58 +0800

    Enhance SunlightEnhancement with caching and logging
    Refactor SunlightEnhancement class to cache enabled state and write to command path.
    
    Signed-off-by: cevente <cjgrenlund@gmail.com>

--- livedisplay/SunlightEnhancement.cpp
+++ livedisplay/SunlightEnhancement.cpp
@@ -3,24 +3,38 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#pragma once
+#define LOG_TAG "SunlightEnhancementService"
 
-#include <aidl/vendor/lineage/livedisplay/BnSunlightEnhancement.h>
+#include <android-base/file.h>
+#include <android-base/logging.h>
+#include <android-base/strings.h>
+
+#include "SunlightEnhancement.h"
 
 namespace aidl {
 namespace vendor {
 namespace lineage {
 namespace livedisplay {
 
-class SunlightEnhancement : public BnSunlightEnhancement {
-  public:
-    // Methods from ::aidl::vendor::lineage::livedisplay::BnSunlightEnhancement follow.
-    ndk::ScopedAStatus getEnabled(bool* _aidl_return) override;
-    ndk::ScopedAStatus setEnabled(bool enabled) override;
+static constexpr const char* kDispCommandPath = 
+        "/proc/mi_display/tx_cmd_set_prim";
 
-  private:
-    bool mEnabled = false;
-};
+ndk::ScopedAStatus SunlightEnhancement::getEnabled(bool* _aidl_return) {
+    // Return the cached state instead of reading the file
+    *_aidl_return = mEnabled;
+    return ndk::ScopedAStatus::ok();
+}
+
+ndk::ScopedAStatus SunlightEnhancement::setEnabled(bool enabled) {
+    // Note the added \n
+    if (!android::base::WriteStringToFile((enabled ? "28\n" : "34\n"), kDispCommandPath)) {
+        LOG(ERROR) << "Failed to write " << kDispCommandPath;
+        return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+    }
+    // Update the cache if the write was successful
+    mEnabled = enabled;
+    return ndk::ScopedAStatus::ok();
+}
 
 }  // namespace livedisplay
 }  // namespace lineage
