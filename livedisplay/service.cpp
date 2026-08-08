7bac1f7370d1606df80dfc0db1c2aecdf84a5af9
cevente <cjgrenlund@gmail.com>
2026-08-08 11:48:35 +0800

    Update service.cpp
    Signed-off-by: cevente <cjgrenlund@gmail.com>

--- livedisplay/service.cpp
+++ livedisplay/service.cpp
@@ -74,10 +74,18 @@ void watchRefreshRateAndTune(std::shared_ptr<SDMController> controller) {
                 HSIC hsic;
                 if (current_fps == 90) {
                     LOG(INFO) << "90Hz active: adjusting SDM hue to 5.";
-                    hsic = {5.0f, 0.0f, 0.0f, 0.0f, 0.0f};
+                    hsic.hue = 5.0f;
+                    hsic.saturation = 0.0f;
+                    hsic.intensity = 0.0f;
+                    hsic.contrast = 0.0f;
+                    hsic.saturationThreshold = 0.0f;
                 } else {
                     LOG(INFO) << "Restoring default SDM profile for " << current_fps << "Hz.";
-                    hsic = {0.0f, 0.0f, 0.0f, 0.0f, 0.0f};
+                    hsic.hue = 0.0f;
+                    hsic.saturation = 0.0f;
+                    hsic.intensity = 0.0f;
+                    hsic.contrast = 0.0f;
+                    hsic.saturationThreshold = 0.0f;
                 }
                 controller->setHSIC(hsic);
             }
