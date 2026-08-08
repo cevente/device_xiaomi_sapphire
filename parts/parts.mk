7aa78c1bd06cb097d3f5cf758a6457cb78dd6c87
cevente <cjgrenlund@gmail.com>
2026-08-08 23:33:02 +0800

    Add files via upload
    Signed-off-by: cevente <cjgrenlund@gmail.com>

---
+++ parts/parts.mk
@@ -0,0 +1,8 @@
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    device/xiaomi/sapphire/parts/sepolicy/private
+
+# BOARD_VENDOR_SEPOLICY_DIRS += \
+ # device/xiaomi/sapphire/parts/sepolicy/vendor
