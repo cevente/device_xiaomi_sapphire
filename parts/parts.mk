fc91baf2de8571335416af880a336f8ef8e4bdda
cevente <cjgrenlund@gmail.com>
2026-08-09 00:25:55 +0800

    Disable private sepolicy directory for Xiaomi parts
    Comment out SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS in parts.mk
    
    Signed-off-by: cevente <cjgrenlund@gmail.com>

--- parts/parts.mk
+++ parts/parts.mk
@@ -1,8 +1,8 @@
 PRODUCT_PACKAGES += \
     XiaomiParts
 
-SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/xiaomi/sapphire/parts/sepolicy/private
+# SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+#    device/xiaomi/sapphire/parts/sepolicy/private
 
 # BOARD_VENDOR_SEPOLICY_DIRS += \
  # device/xiaomi/sapphire/parts/sepolicy/vendor
