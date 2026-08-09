f85b7a45e229c1c5c2fba6b3ccf0d965f4384fb0
cevente <cjgrenlund@gmail.com>
2026-08-09 09:35:13 +0800

    Fix library loading in TouchManager
    Updated library loading to remove 'lib' prefix for Android.
    
    Signed-off-by: cevente <cjgrenlund@gmail.com>

--- parts/src/com/xiaomi/parts/touch/TouchManager.kt
+++ parts/src/com/xiaomi/parts/touch/TouchManager.kt
@@ -6,7 +6,6 @@ class TouchManager {
     companion object {
         private const val TAG = "TouchManager"
 
-        // Mode type mappings from xiaomi_touch.h[span_2](start_span)[span_2](end_span)
         const val TOUCH_GAME_MODE = 0
         const val TOUCH_EXPERT_MODE = 6
         const val TOUCH_RESIST_RF = 12
@@ -15,7 +14,8 @@ class TouchManager {
 
         init {
             try {
-                System.loadLibrary("libxiaomiparts_touch_jni")
+                // Fixed: Do not include the "lib" prefix here; Android appends it automatically.
+                System.loadLibrary("xiaomiparts_touch_jni")
             } catch (e: UnsatisfiedLinkError) {
                 Log.e(TAG, "Failed to load touch jni library", e)
             }
